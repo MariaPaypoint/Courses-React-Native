c4a21fc2515ec4bc0af9e449fb830b9ba3dc1dbf
Maria <maria@paypoint.pro>
2025-01-07 22:15:30 +0300

    start screen layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,16 @@
-import { Text, View, Pressable, ImageBackground } from "react-native";
-import { Link, Stack } from "expo-router";
+import { Text, View, Image, ImageBackground, TouchableOpacity, StyleSheet, Dimensions } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+const windowWidth = Dimensions.get("window").width;
 
 export default function Index() {
+  const router = useRouter();
+
   return (
     <>
       <Stack.Screen options={{ headerShown: false }} />
       <ImageBackground
-        source={require("../assets/images/hello-bg.png")}
+        source={require("@/assets/images/hello-bg.png")}
         style={{
           flex: 1,
         }}
@@ -15,24 +19,59 @@ export default function Index() {
         <View
           style={{
             flex: 1,
-            alignItems: "center",
-            justifyContent: "center",
+            alignItems: "stretch",
+            justifyContent: "space-between",
           }}
         >
-          <Text style={{ color: "white" }}>Edit app/index.tsx to edit this screen.</Text>
-          <Link href="/about" asChild>
-            <Pressable
-              style={{
-                marginTop: 20,
-                padding: 10,
-                borderRadius: 5,
-              }}
-            >
-              <Text style={{ color: "white" }}>About</Text>
-            </Pressable>
-          </Link>
+          <Image source={require("@/assets/images/logo-for-light.png")} style={styles.logo} resizeMode="contain" />
+          <View style={styles.buttonsContainer}>
+            <TouchableOpacity style={[styles.button, styles.buttonMain]} onPress={() => router.push("/about")}>
+              <Text style={[styles.text, styles.textMain]}>{"Let's get started"}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, styles.buttonAdv]} onPress={() => alert("Pressed!")}>
+              <Text style={[styles.text, styles.textAdv]}>{"I already have an account"}</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </ImageBackground>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonsContainer: {
+    marginBottom: 40,
+    marginHorizontal: 24,
+    gap: 16,
+  },
+  button: {
+    alignItems: "center",
+    borderRadius: 8,
+    paddingVertical: 20,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 600,
+  },
+  buttonMain: {
+    backgroundColor: "white",
+  },
+  textMain: {
+    color: "#1C1F40",
+  },
+  buttonAdv: {
+    backgroundColor: "transparent",
+    borderColor: "white",
+    borderWidth: 1,
+    marginBottom: 19,
+  },
+  textAdv: {
+    color: "white",
+  },
+  logo: {
+    width: Math.min(windowWidth * 0.5, 300),
+    height: 100,
+    alignSelf: "center",
+    marginTop: 60,
+  },
+});
